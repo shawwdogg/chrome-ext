e359327ed99b60c4373600b9baf4c90aa65d75d0
Carlos <cshaw.prsvr@gmail.com>
2022-11-21 20:33:59 -0600

    11-21-22

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
+const ulEl =
 
 inputBtn.addEventListener("click", function() {
-
-
-
-
 myLeads.push(inputEl.value)
 console.log(myLeads)
 
 })
 
+
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}
